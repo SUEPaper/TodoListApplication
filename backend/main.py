7ee143a59235a7563f4ddf9a020ecf3b4485084a
HaiwenZhang <haiwensummer@gmail.com>
2023-06-18 16:58:19 +0800

    Change listen host to 127.0.0.1

--- backend/main.py
+++ backend/main.py
@@ -20,4 +20,4 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8000)
