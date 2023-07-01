3d7df3714641c11a6e3b3674d62db98cfce0c634
HaiwenZhang <haiwensummer@gmail.com>
2023-07-01 16:29:09 +0800

    Finished demo migration

--- backend/api/todos.py
+++ backend/api/todos.py
@@ -1,65 +1,31 @@
-from fastapi import APIRouter
-from schemas import todo as schemas_todo
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from typing import Optional
+from api import deps
+from crud import crud_todo
+from schemas import todo as schemas_todo
 
 router = APIRouter()
 
-TODOS = [
-    {
-        "id": 1,
-        "content": "Init todo1",
-        "is_done": False
-    },
-    {
-        "id": 2,
-        "content": "Init todo2",
-        "is_done": False
-    },
-]
-
-
 @router.get("/todos", response_model=list[schemas_todo.Todo])
-def get_all_todos():
-    return TODOS
-
+def get_all_todos(
+    db: Session = Depends(deps.get_db)
+):
+    todos = crud_todo.get_all(db=db)
+    return todos
 
 @router.get("/todos/{todo_id}", response_model=schemas_todo.Todo)
-def get_todo_by_id(todo_id: int):
-    result = None
-    for todo in TODOS:
-        if todo["id"] == todo_id:
-            result = todo
-
-    if result:
-        return result
-    
-@router.get("/todos/search/",  response_model=list[schemas_todo.Todo])
-def search_todos(
-    keyword: Optional[str] = None, max_results: Optional[int] = 10  
+def get_todo_by_id( 
+    todo_id: int,
+    db: Session = Depends(deps.get_db)
 ):
-    """
-    Search for todos based on keyword
-    """
-    if not keyword:
-        # we use Python list slicing to limit results
-        # based on the max_results query parameter
-        return {"results": TODOS[:max_results]} 
-
-    results = filter(lambda todo: keyword.lower() in todo["content"].lower(), TODOS)  
-    return {"results": list(results)[:max_results]}
-
+    todo = crud_todo.get_by_id(db=db, id=todo_id)
+    return todo
+    
 @router.post("/todos", response_model=schemas_todo.Todo)
-def create_todo(*, todo_in: schemas_todo.TodoCreate):  # 2
-    """
-    Create a new todo (in memory only)
-    """
-    new_entry_id = len(TODOS) + 1
-    todo_entry = schemas_todo.Todo(
-        id=new_entry_id,
-        content=todo_in.content,
-        is_done=todo_in.is_done
-    )
-    TODOS.append(todo_entry.dict())  # 3
-
-    return todo_entry
+def create_todo(
+    todo_parmas: schemas_todo.TodoCreate,
+    db: Session = Depends(deps.get_db)
+): 
+    todo = crud_todo.create(db=db, todo_params=todo_parmas)
+    return todo
