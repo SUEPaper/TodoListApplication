024eb311d256c8d31055a299d75aa80b9a41a798
HaiwenZhang <haiwensummer@gmail.com>
2023-07-01 16:43:56 +0800

    Finished put delete

--- backend/api/todos.py
+++ backend/api/todos.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from api import deps
@@ -7,14 +7,14 @@ from schemas import todo as schemas_todo
 
 router = APIRouter()
 
-@router.get("/todos", response_model=list[schemas_todo.Todo])
+@router.get("/todos", response_model=list[schemas_todo.TodoInDB])
 def get_all_todos(
     db: Session = Depends(deps.get_db)
 ):
     todos = crud_todo.get_all(db=db)
     return todos
 
-@router.get("/todos/{todo_id}", response_model=schemas_todo.Todo)
+@router.get("/todos/{todo_id}", response_model=schemas_todo.TodoInDB)
 def get_todo_by_id( 
     todo_id: int,
     db: Session = Depends(deps.get_db)
@@ -22,10 +22,39 @@ def get_todo_by_id(
     todo = crud_todo.get_by_id(db=db, id=todo_id)
     return todo
     
-@router.post("/todos", response_model=schemas_todo.Todo)
+@router.post("/todos", response_model=schemas_todo.TodoInDB)
 def create_todo(
     todo_parmas: schemas_todo.TodoCreate,
     db: Session = Depends(deps.get_db)
 ): 
     todo = crud_todo.create(db=db, todo_params=todo_parmas)
+    return todo
+
+@router.put("/todos/{todo_id}", response_model=schemas_todo.TodoInDB)
+def update_todo(
+    todo_id: int,
+    todo_params: schemas_todo.TodoCreate,
+    db: Session = Depends(deps.get_db)
+):
+    todo = crud_todo.get_by_id(db=db, id=todo_id)
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+    todo = crud_todo.update(db=db, id=todo_id, todo_params=todo_params)
+    return todo
+
+
+@router.delete("/todos/{todo_id}", response_model=schemas_todo.TodoInDB)
+def delete_todo(
+    todo_id: int,
+    db: Session = Depends(deps.get_db)
+):
+
+    todo = crud_todo.get_by_id(db=db, id=todo_id)
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    todo = crud_todo.remove(db=db, id=todo_id)
+
     return todo
